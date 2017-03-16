2328043673d962682b7ca37eeb3de6d53b48a226
JayanthRR <htnayajregatti@gmail.com>
2017-03-16 06:47:47 -0400

    two stage model added in main and results

--- word2vec/main.py
+++ word2vec/main.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 
 from nltk.tokenize import RegexpTokenizer
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from utils.dataset import DataSet
 from utils.generate_test_splits import kfold_split, get_stances_for_folds
@@ -10,7 +10,7 @@ from utils.score import report_score, LABELS, score_submission
 import gensim
 from gensim.models import word2vec
 from scipy.spatial.distance import cosine
-
+from sklearn import svm
 
 
 def cleantext(text):
@@ -31,10 +31,16 @@ def buildWordVector(text, model):
     
     return v
 
-def generate_features(stances, dataset, name, model):
+def generate_features(stances, dataset, name, model, binary=True):
     h, b, y = [], [], [] 
     for stance in stances:
-        y.append(LABELS.index(stance['Stance']))
+        if (binary!=True):
+            y.append(LABELS.index(stance['Stance']))
+        else:
+            if LABELS.index(stance['Stance'])<3:
+                y.append(0)
+            else:
+                y.append(1)
         h.append(buildWordVector(stance['Headline'],model))
         b.append(buildWordVector(dataset.articles[stance['Body ID']], model))
     c = np.c_[h,b]
@@ -60,55 +66,119 @@ if __name__ == "__main__":
     Xbs = dict()
     ys = dict()
     Xcs = dict()
-
+    Xhs_nb = dict()
+    Xbs_nb = dict()
+    ys_nb = dict()
+    Xcs_nb = dict()
+    fold_stances_nb = dict()
+    ys_true=dict()
     print('calculating features')
-    Xc_holdout, Xh_holdout, Xb_holdout, y_holdout = generate_features(hold_out_stances,d,"holdout", model)
+    Xc_holdout, Xh_holdout, Xb_holdout, y_holdout = generate_features(hold_out_stances,d,"holdout", model, binary=False)
     for fold in fold_stances:
-        Xcs[fold], Xhs[fold], Xbs[fold], ys[fold] = generate_features(fold_stances[fold],d,str(fold), model)
+        Xcs[fold], Xhs[fold], Xbs[fold], ys[fold] = generate_features(fold_stances[fold],d,str(fold), model, binary=True)
+        _,_,_,ys_true[fold] = generate_features(fold_stances[fold],d,str(fold),model, binary=False)
+
+    binary_flag=dict()
+    binary_ind = dict()
+    for fold in fold_stances:
+        binary_flag[fold] = [1 if x['Stance']=='unrelated' else 0 for x in fold_stances[fold]]
+        binary_ind[fold] = [i for i,e in enumerate(binary_flag[fold]) if e==0]
+
+    #Xc_holdout_nb, Xh_holdout_nb, Xb_holdout_nb, y_holdout_nb = generate_features(hold_out_stances,d,"holdout",model,binary=False)
+    for fold in fold_stances:
+        fold_stances_nb[fold] = [fold_stances[fold][x] for x in binary_ind[fold]]
+        Xcs_nb[fold], Xhs_nb[fold], Xbs_nb[fold], ys_nb[fold] = generate_features(fold_stances_nb[fold],d,str(fold), model, binary=False)
+
 
     best_score = 0
-    best_fold = None
-
+    best_fold1= None
+    best_fold2= None
     del model
     print('training')
+    #GradientBoostingClassifier already done.
+    #RandomForest Classifier
     for fold in fold_stances:
         ids = list(range(len(folds)))
         del ids[fold]
 
         X_train = np.vstack(tuple([Xcs[i] for i in ids]))
         y_train = np.hstack(tuple([ys[i] for i in ids]))
-
+        
         X_test = Xcs[fold]
         y_test = ys[fold]
-
-        clf = GradientBoostingClassifier(n_estimators=200, random_state=14128, verbose=True)
+        y_test_true = ys_true[fold]
+        #clf = GradientBoostingClassifier(n_estimators=200, random_state=14128, verbose=True)
         #clf = GradientBoostingClassifier(n_estimators=50, random_state=14128, verbose=False)
-        #del model
         #clf = LogisticRegression()
+        clf = RandomForestClassifier(n_estimators=200, n_jobs=4, verbose = True)
+        #clf = svm.SVC(kernel = 'rbf', gamma=0.5, C=1, verbose=True)
         clf.fit(X_train, y_train)
-        # Try random forest
-        #clf.fit(X_train, y_train)
 
-        predicted = [LABELS[int(a)] for a in clf.predict(X_test)]
-        actual = [LABELS[int(a)] for a in y_test]
 
-        fold_score, _ = score_submission(actual, predicted)
+        X_train_nb = np.vstack(tuple([Xcs_nb[i] for i in ids]))
+        y_train_nb = np.hstack(tuple([ys_nb[i] for i in ids]))
+ 
+        clf2 = RandomForestClassifier(n_estimators=200,n_jobs=4,verbose=True)
+        clf2.fit(X_train_nb,y_train_nb)                            
+                
+        
+        predicted = [LABELS[3] if a==1 else LABELS[0] for a in clf.predict(X_test)]
+        actual = [LABELS[int(a)] for a in y_test_true]
+
+        init_pred = dict()
+        init_pred_ind=dict()
+        #fold_stances_new=dict()
+            
+        init_pred[fold] = [int(a) for a in clf.predict(Xcs[fold])]
+        init_pred_ind[fold] = [i for i,e in enumerate(init_pred[fold]) if e==0]
+        
+        Xcs_temp = [Xcs[fold][x] for x in init_pred_ind[fold]]
+        predicted_new = [LABELS[int(a)] for a in clf2.predict(Xcs_temp)]
+        final = predicted
+        for i,e in enumerate(init_pred_ind[fold]):
+            final[e] = predicted_new[i]
+
+
+        fold_score, _ = score_submission(actual, final)
         max_fold_score, _ = score_submission(actual, actual)
-
         score = fold_score/max_fold_score
+
+        #for f in ids:
+
+        #    init_pred[f] = [int(a) for a in clf.predict(Xcs[f])]
+        #    init_pred_ind[f] = [i for i,e in enumerate(init_pred[f]) if e==0]
+        #    fold_stances_new[f] = [fold_stances[1][x]['Stance'] for x in init_pred_ind[f]]
+        #    Xcs_new[f],Xhs_new[f],Xbs_new[f],ys_new[f]=generate_features(fold_stances_new[f],d,str(f),model,binary=False)
+
+
+
 
         print("Score for fold "+ str(fold) + " was - " + str(score))
         if score > best_score:
             best_score = score
-            best_fold = clf
-
+            best_fold1 = clf
+            best_fold2 = clf2
 
 
     #Run on Holdout set and report the final score on the holdout set
-    predicted = [LABELS[int(a)] for a in best_fold.predict(Xc_holdout)]
-    actual = [LABELS[int(a)] for a in y_holdout]
+    predicted = [LABELS[3] if a==1 else LABELS[0] for a in best_fold1.predict(Xc_holdout)]
+    test_pred = dict()
+    test_pred_ind = dict()
+    test_pred = [int(a) for a in best_fold1.predict(Xc_holdout)]
+    test_pred_ind = [i for i,e in enumerate(test_pred) if e==0]
+    
+    Xc_holdout_new = [Xc_holdout[x] for x in test_pred_ind]
+    test_pred_new = [LABELS[int(a)] for a in best_fold2.predict(Xc_holdout_new)]
+    final = predicted
+    for i,e in enumerate(test_pred_ind):
+        final[e] = test_pred_new[i]
 
-    report_score(actual,predicted)
+    actual = [LABELS[int(a)] for a in y_holdout]
+    print('confusion matrix for randomforestclassifier')
+    report_score(actual,final)
+
+
+    #SVM
 
 
 
